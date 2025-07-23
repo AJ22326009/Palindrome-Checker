df01be121984291da207f254ed0255986d062337
aj22326009 <aj22326009@utg.edu.gm>
2025-07-23 08:47:52 +0000

    two re-usable functions added

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const textElement=document.getElementById('text-input');
 const checkBtn=document.getElementById('check-btn');
 const resultShowElement=document.getElementById('result');
 const form=document.getElementById('form');
+
+function formatText(str){
+  return str.replace(/\s/g, '').replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+}
+
+function reverseSpelling(str){
+  const arr=str.split('');
+  const arrReversed=arr.reverse();
+  const strReversed=arrReversed.join('');
+
+  return strReversed;
+}
