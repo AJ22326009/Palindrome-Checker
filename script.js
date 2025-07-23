fb830c6501bee64d53dd13657cc80c51ef7f0093
aj22326009 <aj22326009@utg.edu.gm>
2025-07-23 08:46:53 +0000

    global constants declared

---
+++ script.js
@@ -0,0 +1,4 @@
+const textElement=document.getElementById('text-input');
+const checkBtn=document.getElementById('check-btn');
+const resultShowElement=document.getElementById('result');
+const form=document.getElementById('form');
